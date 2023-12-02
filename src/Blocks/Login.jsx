90d7c7272dd075ae5d302d14d5ca48f88e73db6d
jawad Bin Azam <abidibnazam@gmail.com>
2023-12-02 12:43:56 +0600

    login front-end done

--- src/Blocks/Login.jsx
+++ src/Blocks/Login.jsx
@@ -10,9 +10,20 @@ const Login = () => {
   return (
     <div className="h-screen bg-[#fffafa]">
       {/* hero section and user authentication */}
-      <div className="grid lg:grid-cols-2 grid-cols-1 gap-5">
+      <div className="grid lg:grid-cols-2 grid-cols-1 items-start">
         {/* left portion: graphics */}
-        <div></div>
+        <div className=" py-20 px-10">
+          <div className="flex justify-center">
+            <img className="h-72" src="/Tech Life -tiny.png" alt="Tech life" />
+          </div>
+          <h1 className="text-2xl font-bold text-slate-700 pt-4 text-center">
+            First, Efficient and Productive
+          </h1>
+          <p className="text-xs text-center px-10 pt-2">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis alias
+            esse assumenda natus neque consequatur!
+          </p>
+        </div>
 
         {/* right portion: login form and others */}
         <div className="bg-[#f0fff0] px-10 pt-24 h-screen">
@@ -40,6 +51,7 @@ const Login = () => {
                   {...register("email")}
                 />
                 <input
+                  type="password"
                   placeholder="Password"
                   className="border border-[#e2e2e2] rounded-md pt-2 pb-1 px-2"
                   {...register("pass")}
@@ -62,7 +74,7 @@ const Login = () => {
 
               <div className="px-10">
                 <button
-                  className="w-full pt-2 pb-1 px-7 mt-5 border border-slate-100 bg-[#f6ee86] hover:bg-yellow-300 rounded-md text-sm font-bold text-slate-600"
+                  className="w-full pt-2 pb-1 px-7 mt-5 border border-slate-500 bg-[#f6ee86] hover:bg-yellow-300 rounded-md text-sm font-bold text-slate-600"
                   type="submit"
                 >
                   Sign In
@@ -73,8 +85,8 @@ const Login = () => {
 
           <div className="divider text-sm font-bold text-slate-300">or</div>
           {/* sign in with goole */}
-          <button className="px-4 pt-2 pb-1 rounded-3xl bg-white text-sm font-semibold mx-auto flex justify-center gap-2 my-4">
-            Sign in with Google{" "}
+          <button className="px-4 pt-2 pb-1 rounded-3xl bg-white text-sm font-semibold mx-auto flex justify-center gap-2 my-4 border border-slate-500">
+            Sign in with Google
             <img
               className="h-4 inline-block"
               src="/google-icon-tiny.png"
