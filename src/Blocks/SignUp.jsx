47cac056af988f7d5e0f237415e3b189227ac246
jawad Bin Azam <abidibnazam@gmail.com>
2023-12-03 22:19:36 +0600

    signup ui fixed

--- src/Blocks/SignUp.jsx
+++ src/Blocks/SignUp.jsx
@@ -51,165 +51,174 @@ const SignUp = () => {
   };
 
   return (
-    <div className="grid lg:grid-cols-2 grid-cols-1 items-center">
-      <Toaster position="top-center" richColors />
+    <div>
+      <Toaster position="top-center" richColors />;
+      <div className="grid lg:grid-cols-2 grid-cols-1 items-center lg:h-screen">
+        {/* first grid column */}
+        <div className="bg-[#F8F8FF] py-10 px-20 lg:min-h-screen">
+          <h1 className="text-2xl font-bold text-slate-700">
+            Register / Sign Up
+          </h1>
+          <p className="text-xs pb-4 pt-1">
+            Create your account first, provide all the valid info. <br />
+            All the fields are required
+          </p>
 
-      {/* first grid column */}
-      <div className="bg-[#F8F8FF] py-10 px-20 lg:min-h-screen">
-        <h1 className="text-2xl font-bold text-slate-700">
-          Register / Sign Up
-        </h1>
-        <p className="text-xs pb-4 pt-1">
-          Create your account first, provide all the valid info. <br />
-          All the fields are required
-        </p>
-
-        {/* form */}
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="flex flex-col gap-y-3">
-            <input
-              type="text"
-              placeholder="Full Name"
-              className="input input-bordered w-full"
-              {...register("fullName", { required: true })}
-            />
-            <input
-              type="text"
-              placeholder="Username"
-              className="input input-bordered w-full"
-              {...register("userName", { required: true })}
-            />
-          </div>
-          <div className="flex flex-col gap-3 mt-2">
-            <input
-              type="text"
-              placeholder="Country"
-              className="input input-bordered w-full"
-              {...register("country", { required: true })}
-            />
-            <input
-              type="text"
-              placeholder="Phone Number"
-              className="input input-bordered w-full"
-              {...register("phone", { required: true })}
-            />
-            <input
-              type="email"
-              placeholder="Email"
-              className="input input-bordered w-full"
-              {...register("email", { required: true })}
-            />
-          </div>
-          {/* password */}
-          <div className="mt-2 flex flex-col gap-y-2">
-            <div className="relative">
+          {/* form */}
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-y-3">
               <input
-                type={showPass ? "text" : "password"}
-                placeholder="Password"
+                type="text"
+                placeholder="Full Name"
                 className="input input-bordered w-full"
-                {...register("password", {
-                  required: true,
-                  minLength: 6,
-                  pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/,
-                })}
+                {...register("fullName", { required: true })}
               />
-              <button
-                onClick={toggler}
-                className="absolute right-0 top-2 text-md px-2 pt-2 pb-1 rounded-tr-md rounded-br-md"
-              >
-                {showPass ? "🕶️" : "👀"}
-              </button>
-            </div>
-
-            {/* validations for ui */}
-            {errors.password?.type == "minLength" && (
-              <p className="text-xs font-bold pt-1 text-red-400 mt-1">
-                Password must be atleast 6 characters
-              </p>
-            )}
-            {errors.password?.type === "required" && (
-              <p className="text-xs font-bold pt-1 text-red-400 mt-1">
-                Password is required
-              </p>
-            )}
-            {errors.password?.type === "pattern" && (
-              <p className="text-xs font-bold pt-1 text-red-400 mt-1">
-                uppercase, number and special character required
-              </p>
-            )}
-            <div className="relative">
               <input
-                type={showPass ? "text" : "password"}
-                placeholder="Confirm Password"
-                className="input input-bordered w-full "
-                {...register("confirmPassword", {
-                  required: true,
-                })}
+                type="text"
+                placeholder="Username"
+                className="input input-bordered w-full"
+                {...register("userName", { required: true })}
               />
-              <button
-                onClick={toggler}
-                className="absolute right-0 top-2 text-md px-2 pt-2 pb-1 rounded-tr-md rounded-br-md"
-              >
-                {showPass ? "🕶️" : "👀"}
-              </button>
+            </div>
+            <div className="flex flex-col gap-3 mt-2">
+              <input
+                type="text"
+                placeholder="Country"
+                className="input input-bordered w-full"
+                {...register("country", { required: true })}
+              />
+              <input
+                type="text"
+                placeholder="Phone Number"
+                className="input input-bordered w-full"
+                {...register("phone", { required: true })}
+              />
+              <input
+                type="email"
+                placeholder="Email"
+                className="input input-bordered w-full"
+                {...register("email", { required: true })}
+              />
+            </div>
+            {/* password */}
+            <div className="mt-2 flex flex-col gap-y-2">
+              <div className="relative">
+                <input
+                  type={showPass ? "text" : "password"}
+                  placeholder="Password"
+                  className="input input-bordered w-full"
+                  {...register("password", {
+                    required: true,
+                    minLength: 6,
+                    pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/,
+                  })}
+                />
+                <button
+                  onClick={toggler}
+                  className="absolute right-0 top-2 text-md px-2 pt-2 pb-1 rounded-tr-md rounded-br-md"
+                >
+                  {showPass ? "🕶️" : "👀"}
+                </button>
+              </div>
+
+              {/* validations for ui */}
+              {errors.password?.type == "minLength" && (
+                <p className="text-xs font-bold pt-1 text-red-400 mt-1">
+                  Password must be atleast 6 characters
+                </p>
+              )}
+              {errors.password?.type === "required" && (
+                <p className="text-xs font-bold pt-1 text-red-400 mt-1">
+                  Password is required
+                </p>
+              )}
+              {errors.password?.type === "pattern" && (
+                <p className="text-xs font-bold pt-1 text-red-400 mt-1">
+                  uppercase, number and special character required
+                </p>
+              )}
+              <div className="relative">
+                <input
+                  type={showPass ? "text" : "password"}
+                  placeholder="Confirm Password"
+                  className="input input-bordered w-full "
+                  {...register("confirmPassword", {
+                    required: true,
+                  })}
+                />
+                <button
+                  onClick={toggler}
+                  className="absolute right-0 top-2 text-md px-2 pt-2 pb-1 rounded-tr-md rounded-br-md"
+                >
+                  {showPass ? "🕶️" : "👀"}
+                </button>
+              </div>
+              {matchPassword ? (
+                <p className="text-xs font-bold text-emerald-300 pt-1">
+                  Password match
+                </p>
+              ) : (
+                <p className="text-xs font-bold text-red-400 pt-1">
+                  Password don't match
+                </p>
+              )}
             </div>
             {matchPassword ? (
-              <p className="text-xs font-bold text-emerald-300 pt-1">
-                Password match
-              </p>
+              <button
+                type="submit"
+                className="border-2 border-slate-500 rounded-md px-4 pt-2 pb-1 mt-2 bg-indigo-200 hover:bg-indigo-300 font-bold text-sm w-full  btn text-[#111110]"
+              >
+                Sign Up
+              </button>
             ) : (
-              <p className="text-xs font-bold text-red-400 pt-1">
-                Password don't match
-              </p>
+              <button className="btn-disabled border-2 border-slate-500 rounded-md px-4 pt-2 pb-1 mt-2 bg-indigo-200 hover:bg-indigo-300 font-bold text-sm w-full  btn text-[#111110]">
+                Sign Up
+              </button>
             )}
+          </form>
+          <div className="divider text-sm font-bold text-slate-500 mt-5">
+            or
           </div>
+          <div className="my-1">
+            <h1 className="text-xs text-center">
+              Already have an account?{" "}
+              <Link className="text-indigo-500 hover:font-bold" to={"/"}>
+                Log In
+              </Link>{" "}
+              instead
+            </h1>
+          </div>
+          {/* sign in with google */}
           <button
-            type="submit"
-            className="border-2 border-slate-500 rounded-md px-4 pt-2 pb-1 mt-2 bg-indigo-200 hover:bg-indigo-300 font-bold text-sm w-full  btn text-[#111110]"
+            onClick={handleGoogleSignUp}
+            className="px-4 pt-2 pb-1 rounded-3xl bg-white text-sm font-semibold mx-auto flex justify-center gap-2 my-4 border border-slate-500 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 btn"
           >
-            Sign Up
+            Sign Up with Google
+            <img
+              className="h-4 inline-block"
+              src="/google-icon-tiny.png"
+              alt="google icon"
+            />
           </button>
-        </form>
-        <div className="divider text-sm font-bold text-slate-500 mt-5">or</div>
-        <div className="my-1">
-          <h1 className="text-xs text-center">
-            Already have an account?{" "}
-            <Link className="text-indigo-500 hover:font-bold" to={"/"}>
-              Log In
-            </Link>{" "}
-            instead
-          </h1>
         </div>
-        {/* sign in with google */}
-        <button
-          onClick={handleGoogleSignUp}
-          className="px-4 pt-2 pb-1 rounded-3xl bg-white text-sm font-semibold mx-auto flex justify-center gap-2 my-4 border border-slate-500 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 btn"
-        >
-          Sign Up with Google
-          <img
-            className="h-4 inline-block"
-            src="/google-icon-tiny.png"
-            alt="google icon"
-          />
-        </button>
-      </div>
 
-      {/* second grid column */}
-      <div className="py-20 px-10">
-        <div className="flex justify-center">
-          <img
-            className="h-72"
-            src="/Tech-Life-Schedule-Meeting-tiny.png"
-            alt="Tech life"
-          />
+        {/* second grid column */}
+        <div className="py-20 px-10">
+          <div className="flex justify-center">
+            <img
+              className="h-72"
+              src="/Tech-Life-Schedule-Meeting-tiny.png"
+              alt="Tech life"
+            />
+          </div>
+          <h1 className="text-2xl font-bold text-slate-700 pt-4 text-center">
+            First, Efficient and Productive
+          </h1>
+          <p className="text-xs text-center px-10 pt-2">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis alias
+            esse assumenda natus neque consequatur!
+          </p>
         </div>
-        <h1 className="text-2xl font-bold text-slate-700 pt-4 text-center">
-          First, Efficient and Productive
-        </h1>
-        <p className="text-xs text-center px-10 pt-2">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis alias
-          esse assumenda natus neque consequatur!
-        </p>
       </div>
     </div>
   );
