8788217986d3c264db0719ba103dd46f6902f41d
jawad Bin Azam <abidibnazam@gmail.com>
2023-12-10 00:56:10 +0600

    modal data sent to backend

--- src/Componetns/ReqLinkTbRow.jsx
+++ src/Componetns/ReqLinkTbRow.jsx
@@ -1,6 +1,9 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../Poveiders/AuthProvider";
 import { useForm } from "react-hook-form";
+import { parentUrl } from "../Api/baseUrl";
+import axios from "axios";
+import { Toaster, toast } from "sonner";
 
 const ReqLinkTbRow = ({ index, url, lanchDate }) => {
   // logged in user data
@@ -8,81 +11,104 @@ const ReqLinkTbRow = ({ index, url, lanchDate }) => {
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm();
 
   //   submitted data from modal
   const onSubmit = (data) => {
     console.log(data);
+    const payLoad = {
+      reqBackLink: data.requestedBackLink,
+      msg: data.message,
+      status: "isPending",
+      reqUser: user.displayName,
+      reqUserEmail: user.email,
+      reqUserPhone: user?.phone,
+    };
+    axios
+      .post(`${parentUrl}/requested-backlink`, payLoad)
+      .then((res) => {
+        // console.log(res);
+        toast.success("Request Sent");
+      })
+      .catch((error) => {
+        // console.log(error);
+        toast.error(`${error}`);
+      });
+    reset();
   };
 
   return (
-    <tbody>
-      {/* row 1 */}
-      <tr>
-        <th>{index}</th>
-        <td>
-          <button
-            className="btn btn-outline"
-            onClick={() => document.getElementById("my_modal_1").showModal()}
-          >
-            Request Back-Link
-          </button>
-          <dialog id="my_modal_1" className="modal">
-            <div className="modal-box">
-              <div className="">
-                <form onSubmit={handleSubmit(onSubmit)}>
-                  <label className="form-control w-full">
-                    <div className="label">
-                      <span className="label-text">Given Back-Link URL</span>
+    <>
+      <Toaster position="top-center" richColors />
+      <tbody>
+        {/* row 1 */}
+        <tr>
+          <th>{index}</th>
+          <td>
+            <button
+              className="btn btn-outline"
+              onClick={() => document.getElementById("my_modal_1").showModal()}
+            >
+              Request Back-Link
+            </button>
+            <dialog id="my_modal_1" className="modal">
+              <div className="modal-box">
+                <div className="">
+                  <form onSubmit={handleSubmit(onSubmit)}>
+                    <label className="form-control w-full">
+                      <div className="label">
+                        <span className="label-text">Given Back-Link URL</span>
+                      </div>
+                      <input
+                        type="text"
+                        placeholder="Type here"
+                        className="input input-bordered w-full"
+                        {...register("requestedBackLink", {
+                          required: true,
+                          pattern: /^www\..+\.(com|net|org|io|info|co|edu)$/,
+                        })}
+                      />
+                      {errors.requestedBackLink &&
+                        errors.requestedBackLink?.type === "pattern" && (
+                          <span className="text-xs py-2 font-semibold text-red-400">
+                            Start with www. and end with .com or TLDs
+                          </span>
+                        )}
+                    </label>
+                    <label className="form-control pt-1">
+                      <div className="label">
+                        <span className="label-text">Your Message</span>
+                      </div>
+                      <textarea
+                        className="textarea textarea-bordered h-24"
+                        placeholder="Bio"
+                        {...register("message")}
+                      ></textarea>
+                    </label>
+                    {/* if there is a button in form, it will close the modal */}
+                    {/* <button className="btn">Close</button> */}
+                    <div className="flex items-center gap-x-2 mt-3">
+                      <input
+                        className="btn btn-outline"
+                        type="submit"
+                        value={"Send Request"}
+                      />
+                      <form method="dialog">
+                        <button className="btn btn-outline">Close</button>
+                      </form>
                     </div>
-                    <input
-                      type="text"
-                      placeholder="Type here"
-                      className="input input-bordered w-full"
-                      {...register("requestedBackLink", {
-                        required: true,
-                        pattern: /^www\..+\.(com|net|org|io|info|co|edu)$/,
-                      })}
-                    />
-                    {errors.requestedBackLink &&
-                      errors.requestedBackLink?.type === "pattern" && (
-                        <span className="text-xs py-2 font-semibold text-red-400">
-                          Start with www. and end with .com or TLDs
-                        </span>
-                      )}
-                  </label>
-                  <label className="form-control pt-1">
-                    <div className="label">
-                      <span className="label-text">Your Message</span>
-                    </div>
-                    <textarea
-                      className="textarea textarea-bordered h-24"
-                      placeholder="Bio"
-                      {...register("message")}
-                    ></textarea>
-                  </label>
-                  {/* if there is a button in form, it will close the modal */}
-                  {/* <button className="btn">Close</button> */}
-                  <div className="flex items-center gap-x-2 mt-3">
-                    <input
-                      className="btn btn-outline"
-                      type="submit"
-                      value={"Send Request"}
-                    />
-                    <form method="dialog">
-                      <button className="btn btn-outline">Close</button>
-                    </form>
-                  </div>
-                </form>
+                  </form>
+                </div>
               </div>
-            </div>
-          </dialog>
-        </td>
-        <td>{url}</td>
-        <td>{lanchDate}</td>
-      </tr>
-    </tbody>
+            </dialog>
+          </td>
+          <td>{url}</td>
+          <td>{lanchDate}</td>
+        </tr>
+      </tbody>
+    </>
   );
 };
 
