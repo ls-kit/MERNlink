dd76b37ee7035561e39d377cbefe8680f286d88f
jawad Bin Azam <abidibnazam@gmail.com>
2023-12-10 00:33:07 +0600

    modal is working

--- src/Componetns/ReqLinkTbRow.jsx
+++ src/Componetns/ReqLinkTbRow.jsx
@@ -1,9 +1,20 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../Poveiders/AuthProvider";
+import { useForm } from "react-hook-form";
 
 const ReqLinkTbRow = ({ index, url, lanchDate }) => {
   // logged in user data
   const { user } = useContext(AuthContext);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  //   submitted data from modal
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
     <tbody>
@@ -12,21 +23,57 @@ const ReqLinkTbRow = ({ index, url, lanchDate }) => {
         <th>{index}</th>
         <td>
           <button
-            className="btn"
+            className="btn btn-outline"
             onClick={() => document.getElementById("my_modal_1").showModal()}
           >
-            open modal
+            Request Back-Link
           </button>
           <dialog id="my_modal_1" className="modal">
             <div className="modal-box">
-              <h3 className="font-bold text-lg">Hello!</h3>
-              <p className="py-4">
-                Press ESC key or click the button below to close
-              </p>
-              <div className="modal-action">
-                <form method="dialog">
+              <div className="">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <label className="form-control w-full">
+                    <div className="label">
+                      <span className="label-text">Given Back-Link URL</span>
+                    </div>
+                    <input
+                      type="text"
+                      placeholder="Type here"
+                      className="input input-bordered w-full"
+                      {...register("requestedBackLink", {
+                        required: true,
+                        pattern: /^www\..+\.(com|net|org|io|info|co|edu)$/,
+                      })}
+                    />
+                    {errors.requestedBackLink &&
+                      errors.requestedBackLink?.type === "pattern" && (
+                        <span className="text-xs py-2 font-semibold text-red-400">
+                          Start with www. and end with .com or TLDs
+                        </span>
+                      )}
+                  </label>
+                  <label className="form-control pt-1">
+                    <div className="label">
+                      <span className="label-text">Your Message</span>
+                    </div>
+                    <textarea
+                      className="textarea textarea-bordered h-24"
+                      placeholder="Bio"
+                      {...register("message")}
+                    ></textarea>
+                  </label>
                   {/* if there is a button in form, it will close the modal */}
-                  <button className="btn">Close</button>
+                  {/* <button className="btn">Close</button> */}
+                  <div className="flex items-center gap-x-2 mt-3">
+                    <input
+                      className="btn btn-outline"
+                      type="submit"
+                      value={"Send Request"}
+                    />
+                    <form method="dialog">
+                      <button className="btn btn-outline">Close</button>
+                    </form>
+                  </div>
                 </form>
               </div>
             </div>
