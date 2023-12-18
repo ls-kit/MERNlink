11696a3e2724ca21158e88aecb8c4d48f1fd0afe
jawad Bin Azam <abidibnazam@gmail.com>
2023-12-18 14:19:23 +0600

    layout complete

--- src/Blocks/UserDetails.jsx
+++ src/Blocks/UserDetails.jsx
@@ -34,7 +34,7 @@ const UserDetails = () => {
     const payload = { password: data.pass };
     const id = _id;
     axios
-      .patch(`${loacalServerURL}/users/reset/${id}`, payload)
+      .patch(`${parentUrl}/users/reset/${id}`, payload)
       .then((res) => {
         if (res.data.modifiedCount === 1) {
           toast.success(`Password reset successfull`);
@@ -82,14 +82,17 @@ const UserDetails = () => {
             <small>Active Status: {activeStatus}</small> <br />
             <small>Phone: {phone}</small>
           </div>
-          <div className="flex flex-col gap-4 items-start">
-            {/* send mail */}
+        </div>
+      </div>
+      {/* message and reset section */}
+      <div className="px-5">
+        <div className="grid lg:grid-cols-2 grid-cols-1 items-center my-4 border border-slate-300 rounded-lg p-1">
+          {/* send mail */}
+          <div className="px-4 py-4 bg-[#f1f9ec] rounded-s-md flex gap-2 items-center">
             <div>
+              {/* send message button and text */}
               {/* You can open the modal using document.getElementById('ID').showModal() method */}
-              <div
-                className=" tooltip tooltip-open tooltip-right"
-                data-tip="Send Email"
-              >
+              <div className=" tooltip tooltip-right" data-tip="Send Email">
                 <button
                   className="btn btn-outline btn-sm btn-circle bg-yellow-300"
                   onClick={() =>
@@ -99,6 +102,7 @@ const UserDetails = () => {
                   <IoIosSend className="text-xl font-bold" />
                 </button>
               </div>
+
               <dialog id="my_modal_mail" className="modal">
                 <div className="modal-box">
                   <form method="dialog">
@@ -150,118 +154,129 @@ const UserDetails = () => {
                 </div>
               </dialog>
             </div>
-            {/* reset pass */}
+            {/* text */}
             <div>
-              {/* You can open the modal using document.getElementById('ID').showModal() method */}
-              <div
-                className="tooltip tooltip-open tooltip-right"
-                data-tip="Reset Password"
-              >
-                <button
-                  className="btn btn-circle btn-outline btn-sm bg-emerald-300"
-                  onClick={() =>
-                    document.getElementById("my_modal_pass").showModal()
-                  }
-                >
-                  <MdLockReset className="text-2xl font-bold" />
-                </button>
-              </div>
-              <dialog id="my_modal_pass" className="modal">
-                <div className="modal-box">
-                  <form method="dialog">
-                    {/* if there is a button in form, it will close the modal */}
-                    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 hover:bg-red-400">
-                      <img src="/cross.svg" alt="close" />
-                    </button>
-                  </form>
-                  <h3 className="font-bold text-lg pt-3">Password Reset 🔒</h3>
-                  <p className="py-2 text-xs font-bold text-slate-400">
-                    Press ESC key or click on ✕ button to close <br />
-                    <span className="animate-pulse">In progress...</span>
-                  </p>
-                  <form
-                    className="pt-2"
-                    onSubmit={handleSubmit(onSubmitResetPass)}
-                  >
-                    {/* password */}
-                    <div className="mt-2 flex flex-col gap-y-2">
-                      <div className="relative">
-                        <input
-                          type={showPass ? "text" : "password"}
-                          placeholder="Password"
-                          className="input input-bordered w-full"
-                          {...register("pass", {
-                            required: true,
-                            minLength: 6,
-                            pattern:
-                              /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/,
-                          })}
-                        />
-                        <button
-                          onClick={toggler}
-                          className="absolute right-0 top-2 text-md px-2 pt-2 pb-1 rounded-tr-md rounded-br-md"
-                        >
-                          {showPass ? "🕶️" : "👀"}
-                        </button>
-                      </div>
+              <h1 className="text-sm">Send Personalized Email</h1>
+              <p className="text-xs">
+                Say something interesting to {fullName} 👋
+              </p>
+            </div>
+          </div>
 
-                      {/* validations for ui */}
-                      {errors.pass && errors.pass?.type === "minLength" && (
-                        <p className="text-xs font-bold pt-1 text-red-400 mt-1">
-                          Password must be atleast 6 characters
-                        </p>
-                      )}
-                      {errors.pass && errors.password?.type === "required" && (
-                        <p className="text-xs font-bold pt-1 text-red-400 mt-1">
-                          Password is required
-                        </p>
-                      )}
-                      {errors.pass && errors.pass?.type === "pattern" && (
-                        <p className="text-xs font-bold pt-1 text-red-400 mt-1">
-                          uppercase, number and special character required
-                        </p>
-                      )}
-                      <div className="relative">
-                        <input
-                          type={showPass ? "text" : "password"}
-                          placeholder="Confirm Password"
-                          className="input input-bordered w-full "
-                          {...register("confirmPass", {
-                            required: true,
-                          })}
-                        />
-                        <button
-                          onClick={toggler}
-                          className="absolute right-0 top-2 text-md px-2 pt-2 pb-1 rounded-tr-md rounded-br-md"
-                        >
-                          {showPass ? "🕶️" : "👀"}
-                        </button>
-                      </div>
-                      {matchPassword ? (
-                        <p className="text-xs font-bold text-emerald-300 pt-1">
-                          Password match
-                        </p>
-                      ) : (
-                        <p className="text-xs font-bold text-red-400 pt-1">
-                          Password don't match
-                        </p>
-                      )}
+          {/*pass and text */}
+          <div className="px-4 py-4 bg-[#f1f9ec] rounded-e-md flex gap-2 items-center">
+            {/*reset pass */}
+            {/* You can open the modal using document.getElementById('ID').showModal() method */}
+            <div className="tooltip tooltip-right" data-tip="Reset Password">
+              <button
+                className="btn btn-circle btn-outline btn-sm bg-emerald-300"
+                onClick={() =>
+                  document.getElementById("my_modal_pass").showModal()
+                }
+              >
+                <MdLockReset className="text-2xl font-bold" />
+              </button>
+            </div>
+            <dialog id="my_modal_pass" className="modal">
+              <div className="modal-box">
+                <form method="dialog">
+                  {/* if there is a button in form, it will close the modal */}
+                  <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 hover:bg-red-400">
+                    <img src="/cross.svg" alt="close" />
+                  </button>
+                </form>
+                <h3 className="font-bold text-lg pt-3">Password Reset 🔒</h3>
+                <p className="py-2 text-xs font-bold text-slate-400">
+                  Press ESC key or click on ✕ button to close <br />
+                  {/* <span className="animate-pulse">In progress...</span> */}
+                </p>
+                <form
+                  className="pt-2"
+                  onSubmit={handleSubmit(onSubmitResetPass)}
+                >
+                  {/* password */}
+                  <div className="mt-2 flex flex-col gap-y-2">
+                    <div className="relative">
+                      <input
+                        type={showPass ? "text" : "password"}
+                        placeholder="Password"
+                        className="input input-bordered w-full"
+                        {...register("pass", {
+                          required: true,
+                          minLength: 6,
+                          pattern:
+                            /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/,
+                        })}
+                      />
+                      <button
+                        onClick={toggler}
+                        className="absolute right-0 top-2 text-md px-2 pt-2 pb-1 rounded-tr-md rounded-br-md"
+                      >
+                        {showPass ? "🕶️" : "👀"}
+                      </button>
+                    </div>
+
+                    {/* validations for ui */}
+                    {errors.pass && errors.pass?.type === "minLength" && (
+                      <p className="text-xs font-bold pt-1 text-red-400 mt-1">
+                        Password must be atleast 6 characters
+                      </p>
+                    )}
+                    {errors.pass && errors.password?.type === "required" && (
+                      <p className="text-xs font-bold pt-1 text-red-400 mt-1">
+                        Password is required
+                      </p>
+                    )}
+                    {errors.pass && errors.pass?.type === "pattern" && (
+                      <p className="text-xs font-bold pt-1 text-red-400 mt-1">
+                        uppercase, number and special character required
+                      </p>
+                    )}
+                    <div className="relative">
+                      <input
+                        type={showPass ? "text" : "password"}
+                        placeholder="Confirm Password"
+                        className="input input-bordered w-full "
+                        {...register("confirmPass", {
+                          required: true,
+                        })}
+                      />
+                      <button
+                        onClick={toggler}
+                        className="absolute right-0 top-2 text-md px-2 pt-2 pb-1 rounded-tr-md rounded-br-md"
+                      >
+                        {showPass ? "🕶️" : "👀"}
+                      </button>
                     </div>
                     {matchPassword ? (
-                      <button
-                        type="submit"
-                        className="border-2 border-slate-500 rounded-md px-4 pt-2 pb-1 mt-2 bg-indigo-200 hover:bg-indigo-300 font-bold text-sm w-full  btn text-[#111110]"
-                      >
-                        Reset Password
-                      </button>
+                      <p className="text-xs font-bold text-emerald-300 pt-1">
+                        Password match
+                      </p>
                     ) : (
-                      <button className="btn-disabled border-2 border-slate-500 rounded-md px-4 py-2 mt-2 bg-indigo-200 hover:bg-indigo-300 font-bold text-sm w-full  btn text-[#111110]">
-                        Reset Password
-                      </button>
+                      <p className="text-xs font-bold text-red-400 pt-1">
+                        Password don't match
+                      </p>
                     )}
-                  </form>
-                </div>
-              </dialog>
+                  </div>
+                  {matchPassword ? (
+                    <button
+                      type="submit"
+                      className="border-2 border-slate-500 rounded-md px-4 pt-2 pb-1 mt-2 bg-indigo-200 hover:bg-indigo-300 font-bold text-sm w-full  btn text-[#111110]"
+                    >
+                      Reset Password
+                    </button>
+                  ) : (
+                    <button className="btn-disabled border-2 border-slate-500 rounded-md px-4 py-2 mt-2 bg-indigo-200 hover:bg-indigo-300 font-bold text-sm w-full  btn text-[#111110]">
+                      Reset Password
+                    </button>
+                  )}
+                </form>
+              </div>
+            </dialog>
+            {/* text */}
+            <div>
+              <h1 className="text-sm">Reset Password of {fullName}?</h1>
+              <p className="text-xs">P.S: Only admit created users</p>
             </div>
           </div>
         </div>
