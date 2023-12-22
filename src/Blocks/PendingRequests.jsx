0fbc494364b24c2a4c74f195603e5c85ef4c778b
jawad Bin Azam <abidibnazam@gmail.com>
2023-12-22 12:13:43 +0600

    working with pending request

--- src/Blocks/PendingRequests.jsx
+++ src/Blocks/PendingRequests.jsx
@@ -3,8 +3,13 @@ import { parentURL } from "../Api/baseUrl";
 import axios from "axios";
 import { AuthContext } from "../Poveiders/AuthProvider";
 import PendingTableRow from "../Componetns/PendingTableRow";
+import ReactPaginate from "react-paginate";
+import { listOfItemsPerPg } from "../Api/itemsPerPage";
 
 const PendingRequests = () => {
+  const [itemOffset, setItemOffset] = useState(0);
+  const [itemsPerPage, setItemsPerPage] = useState(3);
+  const [search, setSearch] = useState("");
   // get email from Auth
   const { user } = useContext(AuthContext);
   const [pendingData, setPendingData] = useState([]);
@@ -14,11 +19,11 @@ const PendingRequests = () => {
       const res = await axios
         .get(`${parentURL}/requested-backlink/pending/${user?.email}`)
         .then((res) => {
-          console.log(res);
+          // console.log(res);
           setPendingData(res.data);
         })
         .catch((error) => {
-          console.log(error);
+          toast.error(error.message);
         });
       //   setPendingData(res.data);
     };
@@ -26,8 +31,63 @@ const PendingRequests = () => {
     fetchData();
   }, [parentURL]);
 
+  // react pagination
+
+  const endOffset = itemOffset + itemsPerPage;
+  const currentItems = pendingData.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(pendingData.length / itemsPerPage);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % pendingData.length;
+    setItemOffset(newOffset);
+  };
+
+  // select items per page -> by me
+  const handleItemsPerPage = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const itemsPerPg = form.perPageData.value;
+    setItemsPerPage(itemsPerPg);
+  };
+
   return (
     <>
+      {/* navigation */}
+      <div className="py-2 px-3 flex gap-5 items-center">
+        <input
+          type="search"
+          placeholder="www.example.com"
+          onChange={(e) => setSearch(e.target.value)}
+          className="input input-sm  input-bordered w-full max-w-xs"
+        />
+        {/* set items per page */}
+        <form className="flex gap-3" onSubmit={handleItemsPerPage}>
+          <div>
+            <label className="form-control lg:w-fit w-full">
+              <select
+                className="select select-bordered select-sm"
+                name="perPageData"
+              >
+                <option disabled selected>
+                  Items Per Page
+                </option>
+                {listOfItemsPerPg.map((item, i) => (
+                  <option value={item} key={i}>
+                    {item}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </div>
+          <button
+            className="text-xs font-semibold btn btn-sm btn-outline bg-yellow-200"
+            type="submit"
+          >
+            Set items
+          </button>
+        </form>
+      </div>
       <div className="overflow-x-auto font-roboto">
         <table className="table table-sm">
           <thead>
@@ -41,7 +101,7 @@ const PendingRequests = () => {
               <th>Requested By</th>
             </tr>
           </thead>
-          {pendingData.map((pendingItem, i) => (
+          {currentItems.map((pendingItem, i) => (
             <PendingTableRow
               key={i + 0.01}
               index={i + 1}
@@ -55,6 +115,21 @@ const PendingRequests = () => {
           ))}
         </table>
       </div>
+      {/* pagination */}
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel="< prev"
+        renderOnZeroPageCount={null}
+        containerClassName="pagination"
+        previousLinkClassName="direction"
+        pageLinkClassName="page-num"
+        nextLinkClassName="direction"
+        activeLinkClassName="active"
+      />
     </>
   );
 };
